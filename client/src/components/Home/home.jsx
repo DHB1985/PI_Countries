61384256ce2ac0ea33b3d80b9bec4369ceed28a5
DHB1985 <90072728+DHB1985@users.noreply.github.com>
2021-11-21 20:13:42 -0300

    20211121 2013

--- client/src/components/Home/home.jsx
+++ client/src/components/Home/home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getCountries } from "../../redux/actions";
+import {
+  getCountries,
+  filterCountriesByContinent,
+  orderByCountryName,
+} from "../../redux/actions";
 import { Link } from "react-router-dom";
 import CountryCard from "../Card/Card";
 
@@ -19,6 +23,22 @@ const Home = () => {
   const handleClick = (event) => {
     dispatch(getCountries());
     event.preventDefault();
+  }; //Funcion para resetear el State, para que vuelva a traer todos los países
+
+  //Funcion para ejecutar el filtrado por continente
+
+  const handleFilterContinent = (event) => {
+    dispatch(filterCountriesByContinent(event.target.value));
+    event.preventDefault();
+  };
+
+  //Funcion para ejecutar el ordenamiento por nombre
+  const [orden, setOrden] = useState("");
+  const handleOrderByName = (event) => {
+    dispatch(orderByCountryName(event.target.value));
+    setCurrentPage(1);
+    setOrden(`Ordenado ${event.target.value}`);
+    event.preventDefault();
   };
 
   // Estados locales para setear el paginado
@@ -27,13 +47,14 @@ const Home = () => {
   let indexFirstPage = 0; //Para restar 1 país en la primer página y que uestre solo 9
   //Este if es para mostrar 9 en la primer página, si no es la primera va a valer 0, entonces va a mostrar 10
   //Pierdo uno entre la pagina 1 y la 2
-  if (currentPage === 1){
-    indexFirstPage=1
-   } else {
-     indexFirstPage = 0
-   }
+  if (currentPage === 1) {
+    indexFirstPage = 1;
+  } else {
+    indexFirstPage = 0;
+  }
   const indexOfLastCountry = currentPage * (countriesPerPage - indexFirstPage); //Para setear el el índice del último país en la pagina actual
-  const indexOfFirstCountry = indexOfLastCountry - (countriesPerPage-indexFirstPage); // Para setear el índice del primer paíes ne la página
+  const indexOfFirstCountry =
+    indexOfLastCountry - (countriesPerPage - indexFirstPage); // Para setear el índice del primer paíes ne la página
 
   const currentCountries = allCountries.slice(
     indexOfFirstCountry,
@@ -42,6 +63,7 @@ const Home = () => {
   const paged = (pageNumber) => {
     setCurrentPage(pageNumber);
   }; //Setea el número de la página a mostrar
+  //Final de las funciones de paginado
 
   return (
     <div>
@@ -57,15 +79,15 @@ const Home = () => {
 
       {/* Orden Ascendente/Descendente por nombre*/}
       <div>
-        <select>
-          <option value="ascend">Ascendente</option>
-          <option value="descend">Descendente</option>
+        <select onChange={event => handleOrderByName(event)}>
+          <option value="ascendName">Ascendente</option>
+          <option value="descendName">Descendente</option>
         </select>
       </div>
 
       {/* Orden Ascendente/Descendente por cantidad de poblacion*/}
       <div>
-        <select>
+        <select >
           <option value="ascendPob">Pob. Ascendente</option>
           <option value="descendPob">Pob. Descendente</option>
         </select>
@@ -73,19 +95,20 @@ const Home = () => {
 
       {/* Filtrado por Continente */}
       <div>
-        <select>
-          <option value="africa">Africa</option>
-          <option value="america">America</option>
-          <option value="asia">Asia</option>
-          <option value="europa">Europa</option>
-          <option value="oceania">Oceania</option>
+        <select onChange={(event) => handleFilterContinent(event)}>
+          <option value="All">Todos</option>
+          <option value="Africa">Africa</option>
+          <option value="Americas">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europa</option>
+          <option value="Oceania">Oceania</option>
         </select>
       </div>
 
       {/* Filtrado por Actividad */}
       <div>
         <select>
-          <option value="all">Todas</option>
+          <option value="All">Todas</option>
           <option value="actividad1">actividad1</option>
           <option value="actividad2">actividad2</option>
           <option value="actividad3">actividad3</option>
--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,7 +1,12 @@
-import { GETALLCOUNTRIES } from "../actions/constants";
+import {
+  GETALLCOUNTRIES,
+  FILTERBYCONTINENT,
+  ORDERBYCOUNTRYNAME,
+} from "../actions/constants";
 
 const initialState = {
   countries: [],
+  allCountries: [],
 };
 
 const rootReducer = (state = initialState, action) => {
@@ -10,7 +15,49 @@ const rootReducer = (state = initialState, action) => {
       return {
         ...state,
         countries: action.payload,
+        allCountries: action.payload,
       };
+
+    case FILTERBYCONTINENT:
+      let stateFiltered;
+      if (action.payload === "All") {
+        stateFiltered = state.allCountries;
+      } else {
+        stateFiltered = state.allCountries.filter(
+          (value) => value.continent === action.payload
+        );
+      }
+      return {
+        ...state,
+        countries: stateFiltered,
+      };
+
+    case ORDERBYCOUNTRYNAME:
+      let sortedCountries;
+      if (action.payload === "ascendName") {
+        sortedCountries = state.countries.sort((a, b) => {
+          if (a.name > b.name) {
+            return 1;
+          } else if (b.name > a.name) {
+            return -1;
+          }
+          return 0;
+        });
+      } else if (action.payload === "descendName") {
+        sortedCountries = state.countries.sort((a, b) => {
+          if (a.name > b.name) {
+            return -1;
+          } else if (b.name > a.name) {
+            return 1;
+          }
+          return 0;
+        });
+      }
+      return {
+        ...state,
+        countries: sortedCountries,
+      }
+
     default:
       return state;
   }
